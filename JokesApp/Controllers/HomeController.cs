65909e33dd46ec6f933436576c8204317c57cdf0
nareshaspire <Nareshaspire45@gmail.com>
2022-12-10 00:59:55 +0100

    remove privacy action from HomeController

--- JokesApp/Controllers/HomeController.cs
+++ JokesApp/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace JokesApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
