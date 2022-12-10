d55ca75e1492f94d8ce1d248ee81f5804e60abdd
nareshaspire <Nareshaspire45@gmail.com>
2022-12-10 01:22:14 +0100

    add data annotations + refine the ui

--- JokesApp/Models/Joke.cs
+++ JokesApp/Models/Joke.cs
@@ -1,9 +1,16 @@
-﻿namespace JokesApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JokesApp.Models
 {
     public class Joke
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Joke Question")]
         public string JokeQuestion { get; set; }
+        [Required]
+        [Display(Name = "Joke Answer")]
         public string JokeAnswer { get; set; }
     }
 }
